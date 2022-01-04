0d217f2322ba15517518f8e82277463dfb1de9a5
Hitesh Gupta <hitesh.ditstek@ditstek.com>
2022-01-04 11:56:50 +0530

    Add Domain Model

---
+++ app/Models/Domain/Domain.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Domain;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Domain extends Model
+{
+    use HasFactory;
+	protected $guarded = [];
+}
